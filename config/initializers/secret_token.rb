b966a3f2ec9a74fedc2263e182f3be5db3de98ce
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-09-08 16:51:58 -0300

    adding empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmokeSignals::Application.config.secret_token = '756215887f02f9a06ac7769d5650bcc9646fca69aa3910294721b126e6180373eb3399808363007f2d6d3e22e61ef9fdf66ae408926a75fa97453a85f2ac31e2'
