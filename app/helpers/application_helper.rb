f84f84006ebd85dfb3bf2f0f0654ad341274b876
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-09-09 12:07:36 -0300

    added some missing translations and fixed failing tests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_name_to_bootstrap_alert(flash_name)
+    case flash_name
+      when :notice then :success
+      when :alert then :error
+      else flash_name
+    end
+  end
 end
--- test/factories.rb
+++ test/factories.rb
@@ -6,6 +6,9 @@ FactoryGirl.define do
   end
   
   factory :user do
+    sequence(:email) { |n| "user#{n}@gmail.com" }
+    sequence(:first_name) { |n| "Name #{n}" }
+    sequence(:last_name) { |n| "Last name #{n}" }
     provider 'google_oauth2'
     sequence(:uid) { |n| "user#{n}" }
   end
